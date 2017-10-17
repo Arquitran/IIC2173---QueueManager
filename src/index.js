e45bcb04790f821af5bc5cebe8baded0c1a3ba3a
Felipe Garrido <figarrido@uc.cl>
2017-10-17 20:10:00 -0300

    Caching and attempts manage

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ const PORT = 8080
 
 app.use(bodyParser.json())
 
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(204)
+})
+
 router.get('/:endpoint', (req, res) => {
   res.writeHead(200, {
     contentType: 'text/json'
@@ -22,15 +26,27 @@ router.get('/:endpoint', (req, res) => {
   producer.publish('request', {
     id,
     url: req.params.endpoint,
-    query: req.query,
+    query: {
+      page: req.query.page
+    },
     point: req.params.endpoint
   })
 
+  console.log(id)
+
   consumer.on('message', msg => {
     let message = JSON.parse(msg.body.toString())
-
+    console.log(message.id)
     if (message.id != id) return
     msg.finish()
+
+    if (message.data === null) {
+      res.end(JSON.stringify({
+        message: 'FAIL'
+      }))
+      return
+    }
+
     res.end(JSON.stringify({
       data: message.data
     }))
--- src/manager.js
+++ src/manager.js
@@ -1,12 +1,17 @@
 import { Reader, Writer } from 'nsqjs'
 import axios from 'axios'
+import mcache from 'memory-cache'
 
-const requester = axios.create({
-  baseURL: 'http://arqss17.ing.puc.cl:3000/'
-})
-
+const MAX_TIMEOUT = 300
+const MAX_ATTEMPTS = 3
 const NSQ_HOST = 'nsqd'
 const NSQ_PORT = 4150
+const SEC_CACHE = 60
+
+const requester = axios.create({
+  baseURL: 'http://arqss17.ing.puc.cl:3000/',
+  timeout: MAX_TIMEOUT
+})
 
 const writer = new Writer(NSQ_HOST, NSQ_PORT)
 const reader = new Reader('request', 'ch_test', {
@@ -18,15 +23,40 @@ reader.connect()
 
 reader.on(Reader.MESSAGE, msg => {
   const message = JSON.parse(msg.body.toString())
-  requester.get(message.url, {
-    params: message.query
-  })
-    .then(response => {
-      writer.publish('response', {
-        id: message.id,
-        data: response.data
-      })
-      msg.finish()
+  if (msg.attempts > MAX_ATTEMPTS) {
+    // TO-DO: Es posible cachear valores anteriores en caso
+    // de superar el máximo de intentos.
+    console.log(`[ERROR] Max attempts exceeded for ${message.id}`)
+    msg.finish()
+    writer.publish('response', {
+      id: message.id,
+      data: mcache.get(`${message.url}_old`)
     })
-    .catch()
+    return
+  }
+
+  console.log(`[REQUEST] /${message.url} from ${message.id}`)
+  const cachedData = mcache.get(message.url)
+  if (cachedData !== null) {
+    msg.finish()
+    writer.publish('response', {
+      id: message.id,
+      data: cachedData
+    })
+  } else {
+    requester.get(message.url, {params: message.query})
+      .then(response => {
+        mcache.put(message.url, response.data, SEC_CACHE * 1000)
+        mcache.put(`${message.url}_old`, response.data)
+        writer.publish('response', {
+          id: message.id,
+          data: response.data
+        })
+        msg.finish()
+      })
+      .catch(err => {
+        console.log(`[ERROR] ${err.message}`)
+        msg.requeue()
+      })
+  }
 })
